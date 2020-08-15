0ae2f95bbe3989c0e5a0b43e9e4f5772831136f4
Yuval Kirstain <kirstain.yuval@gmail.com>
2020-08-15 16:38:25 +0300

    dumping full example data to pickle for decoding

--- eval.py
+++ eval.py
@@ -6,9 +6,8 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torch import nn
-
 from data import get_dataset
-
+from utils import write_examples, EVAL_DATA_FILE_NAME
 logger = logging.getLogger(__name__)
 
 
@@ -34,13 +33,18 @@ class Evaluator:
         logger.info("  Batch size = %d", self.eval_batch_size)
         model.eval()
 
-        for batch in eval_dataloader:
-            batch = tuple(tensor.to(self.args.device) for tensor in batch)
-            input_ids, attention_mask, start_entity_mentions_indices, end_entity_mentions_indices, start_antecedents_indices, end_antecedents_indices = batch
+        with open(os.path.join(self.eval_output_dir, EVAL_DATA_FILE_NAME), "wb") as f:
+            for batch in eval_dataloader:
+                # batch = tuple(tensor.to(self.args.device) for tensor in batch)
+                input_ids, attention_mask, start_entity_mentions_indices, end_entity_mentions_indices, start_antecedents_indices, end_antecedents_indices, gold_clusters = batch
+                input_ids = input_ids.to(self.args.device)
+                attention_mask = attention_mask.to(self.args.device)
 
-            with torch.no_grad():
-                outputs = model(input_ids, attention_mask=attention_mask, return_all_outputs=True)
-                # TODO: Do something
+                with torch.no_grad():
+                    outputs = model(input_ids, attention_mask=attention_mask, return_all_outputs=True)
+
+                write_examples(f, tuple(tensor.cpu().tolist() for tensor in tuple(batch) + outputs[:3]))
+
 
         results = {}
         logger.info("***** Eval results {} *****".format(prefix))
@@ -61,5 +65,4 @@ class Evaluator:
                     else:
                         writer.write(f"{key} = {values}\n")
 
-        eval_dataset.close()
         return results
